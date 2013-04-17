4a35ca9bea99846ffa035dbc88e4bcba2dce1998
Brunno Gonçalves <brunnocgoncalves@gmail.com>
2013-04-17 10:13:16 -0300

    Added payment page

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -23,6 +23,7 @@
 	function createDataBase(tx) {
         tx.executeSql('CREATE TABLE IF NOT EXISTS S_ALUNO (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, email TEXT, valor TEXT, dataFicha DATETIME, datanascimento DATETIME, pagamento TEXT, tipo TEXT, aulaficha TEXT, created DATETIME, created_by TEXT, updated DATETIME, updated_by TEXT)');
 		tx.executeSql('CREATE TABLE IF NOT EXISTS S_AULA (id INTEGER PRIMARY KEY AUTOINCREMENT, idAluno INTEGER, data DATETIME, created DATETIME, created_by TEXT, updated DATETIME, updated_by TEXT)');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS S_PAGAMENTO (id INTEGER PRIMARY KEY AUTOINCREMENT, idAluno INTEGER, data DATETIME, valor TEXT, created DATETIME, created_by TEXT, updated DATETIME, updated_by TEXT)');
     }
 	
 	function zeraBaseDados(tx) {
@@ -33,6 +34,29 @@
 		tx.executeSql('SELECT S1.nome, S.data FROM S_AULA S INNER JOIN S_ALUNO S1 ON S1.id = S.idAluno Where S.idAluno = ' + idAluno, [], successLoadAluno, errorLoadAluno);
 	}
 	
+	function carregaPagamentoAluno(tx){
+		tx.executeSql('SELECT * FROM S_PAGAMENTO S Where S.idAluno = ' + idAluno, [], successLoadPagamento, errorLoadPagamento);
+	}
+	
+	function successLoadPagamento(tx, results) {
+		console.log('Pagamentos: ' + results.rows.length);
+		var len = results.rows.length;
+		var parent = document.getElementById('listviewPagamentos');
+		for (var i=0; i<len; i++){
+			var listItem = document.createElement('li');
+			listItem.setAttribute('id','listitem');
+			
+			listItem.innerHTML = '<p>Data: ' + results.rows.item(i).data + '</p>'
+							+ '<p class="ui-li-aside"><strong>R$' + results.rows.item(i).valor + '</strong></p>';
+	 
+			parent.appendChild(listItem);
+		}
+		
+		var list = document.getElementById('listviewPagamentos');
+		$(list).listview("refresh");
+		
+    }
+	
 	function successLoadAluno(tx, results) {
 		console.log('Aulas');
 		var len = results.rows.length;
@@ -41,7 +65,7 @@
 			var listItem = document.createElement('li');
 			listItem.setAttribute('id','listitem');
 			
-			listItem.innerHTML = 'Data: ' + results.rows.item(i).data;
+			listItem.innerHTML = '<p>Data: ' + results.rows.item(i).data + '</p>';
 	 
 			parent.appendChild(listItem);
 		}
@@ -49,9 +73,16 @@
 		var list = document.getElementById('listviewAulas');
 		$(list).listview("refresh");
 		
+		var db = window.openDatabase("PersonalProBeta", "1.0", "Personal Pro", 200000);
+		db.transaction(carregaPagamentoAluno, error);
+		
     }
 	
 	function errorLoadAluno(err) {
+        console.log("Error processing SQL: "+err.message);
+    }
+	
+	function errorLoadPagamento(err) {
         console.log("Error processing SQL: "+err.message);
     }
 	
@@ -103,7 +134,7 @@
 			var listItem = document.createElement('li');
 			listItem.setAttribute('id','listitem');
 			
-			listItem.innerHTML = '<a href="#" onclick="openAluno('+results.rows.item(i).id+');");">'
+			listItem.innerHTML = '<a href="#" onclick="openAluno(' +results.rows.item(i).id+ ', \''+ results.rows.item(i).nome +'\');">'
 							+ '<h2>' + results.rows.item(i).nome + '</h2>'
 							+ '<p><strong>Ficha iniciada em ' + results.rows.item(i).dataFicha + ' e irá vencer após ' + results.rows.item(i).aulaficha + ' aulas.</strong></p>'
 							+ '<p>Pagamento a cada ' + results.rows.item(i).pagamento + ' ' + results.rows.item(i).tipo + '.</p>'
@@ -160,7 +191,7 @@
 		var listItem = document.createElement('li');
 		listItem.setAttribute('id','listitem');
 		
-		listItem.innerHTML = '<a href="#" onclick="openAluno('+novoId+');");">'
+		listItem.innerHTML = '<a href="#" onclick="openAluno('+novoId+', \'' + document.getElementById("name").value + '\');">'
 						+ '<h2>' + document.getElementById("name").value + '</h2>'
 						+ '<p><strong>Ficha iniciada em ' + document.getElementById("dataficha").value + ' e irá vencer após ' + document.getElementById("aulaficha").value + ' aulas.</strong></p>'
 						+ '<p>Pagamento a cada ' + document.getElementById("pagamento").value + ' ' + $('#tipo').val() + '.</p>'
@@ -187,7 +218,7 @@
 		varDataFicha = document.getElementById("dataficha").value;
 		varaulaficha = document.getElementById("aulaficha").value;
 		vardatanascimento = document.getElementById("datanascimento").value;
-		varpagamento = document.getElementById("pagamento").value;
+		varpagamento = document.getElementById("formaPagamento").value;
 		vartipo = $('#tipo').val();
         tx.executeSql('INSERT INTO S_ALUNO (nome, email, valor, dataficha, datanascimento, pagamento, tipo, aulaficha, created, created_by) VALUES ("' + varNome + '", "' + varEmail + '", "' + varValor + '", "' + varDataFicha + '", "' + vardatanascimento + '", "' + varpagamento + '", "' + vartipo + '", "' + varaulaficha + '", datetime("now"), "ADMIN")');
     }
@@ -198,8 +229,9 @@
 		db.transaction(listaAlunos, error);
 	} );
 	
-	function openAluno(id){
+	function openAluno(id, nome){
 		idAluno = id;
+		$('#nomeAluno').text(nome);
 		$.mobile.changePage("#pagealuno");
 	}
 	
@@ -240,19 +272,30 @@
 		db.transaction(createAula, errorCAA, successCAA);
 	}
 	
+	function createPagamento(tx) {
+		varAluno = $('#alunoPagamento').val();
+		varData = document.getElementById("dataPagamento").value;
+		varPagamento = document.getElementById("valorPagamento").value;
+        tx.executeSql('INSERT INTO S_PAGAMENTO (idAluno, data, valor, created, created_by) VALUES ("' + varAluno + '", "' + varData + '", "' + varPagamento + '", datetime("now"), "ADMIN")');
+    }
+	
+	function errorCP(err) {
+        console.log("Error processing SQL: "+err.message);
+    }
+
+    // Transaction success callback
+    //
+    function successCP() {
+        alert('Pagamento registrado com sucesso!');
+    }
+	
 	function registraPagamento(){
 		var db = window.openDatabase("PersonalProBeta", "1.0", "Personal Pro", 200000);
 		db.transaction(createPagamento, errorCP, successCP);
 	}
 	
-	$( "#pagamento" ).on( "pagecreate", function( event, ui ) {
+	$( "#pagamento" ).on( "pageshow", function( event, ui ) {
 		console.log('Pagamento');
 		var db = window.openDatabase("PersonalProBeta", "1.0", "Personal Pro", 200000);
 		db.transaction(listaAlunosPagamento, error);
 	} );
-	
-	function pagamento(){
-		$.mobile.changePage("#pagamento");
-	}
-	
-	$( "#pagamento" ).on( "pagebeforecreate", function( event, ui ) {console.log('Before pagamento');} );
