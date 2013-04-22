043d525dc9f6e614debbacd896315056316cdf40
Brunno Gonçalves <brunnocgoncalves@gmail.com>
2013-04-22 20:34:29 -0300

    Fix Java

--- src/com/bzapps/personalProBeta/PersonalProBeta.java
+++ src/com/bzapps/personalProBeta/PersonalProBeta.java
@@ -27,10 +27,8 @@ public class PersonalProBeta extends DroidGap
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        // Set by <content src="index.html" /> in config.xml
-        super.loadUrl(Config.getStartUrl());
-        //super.loadUrl("file:///android_asset/www/index.html")
+    	super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
 
